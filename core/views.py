096b8528c52cd3f1fd68d56ad459ab8e999a8cc0
costar011 <dnaer456@gmail.com>
2021-05-30 15:32:21 +0900

    yyyyyy

--- core/views.py
+++ core/views.py
@@ -4,6 +4,6 @@ from chips import models as chip_models
 
 def homeRender(request):
 
-    chipList = (chip_models.Chip.objects.all(),)
+    chipList = chip_models.Chip.objects.all()
 
     return render(request, "screens/home.html", context={"chipList": chipList})
